e4276cd0bbfa217e99a8938ac604fbbc58bec74b
tomi0505 <tgdevit@gmail.com>
2025-04-04 12:36:10 +0200

    add tasks works

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <form>
+      <div class="mb-3">
+        <input type="email" class="form-control" placeholder="Zadanie do wykonania" v-model="taskName">
+        <div class="form-text" v-show="taskNameIsEmpty">To pole nie może być puste!</div>
+      </div>
+      <button type="button" class="btn btn-primary" @click="addTask">Dodaj</button>
+
+      <ul class="list-group">
+        <TaskItem v-for="task in tasks" :key="task.id" :id="task.id" :name="task.name" />
+      </ul>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskItem from './components/TaskItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TaskItem
+  },
+  data() {
+    return {
+      taskName: '',
+      tasks: [],
+      taskNameIsEmpty: false
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.taskName.trim() !== '') {
+        // NEW TASK PATTERN
+        const task = {
+          id: crypto.randomUUID(),
+          name: this.taskName.trim()
+        }
+        // NEW TASK PATTERN END
+
+        this.tasks.push(task);
+        this.taskName = '';
+        this.taskNameIsEmpty = false;
+      } else {
+        this.taskNameIsEmpty = true;
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #5f9ea0;
 }
 </style>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" :id="id">
+    <label class="form-check-label" :for="id">{{ name }}</label>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    name: String,
+    id: String
+  }
+}
+</script>
